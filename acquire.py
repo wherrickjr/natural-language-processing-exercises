9982f225799dfad53d8de7d2a04a61d8a5212f6d
Bill Herrick <wherrickjr@gmail.com>
2023-02-28 16:50:08 -0600

    finished adding functions

--- acquire.py
+++ acquire.py
@@ -3,6 +3,9 @@ import requests
 import os
 
 def scrape_codeup(url):
+    '''
+    function that scrapes codeups blog site
+    '''
     headers = {'User-Agent': 'Codeup Data Science'}
     response = requests.get(url, headers=headers)
     soup = BeautifulSoup(response.content, 'html.parser')
@@ -23,3 +26,38 @@ def scrape_codeup(url):
         dictionary = {}
         lists.append(dictionary)
     return lists
+
+
+def scrape_one_page(topic):
+    '''
+    function that scrapes articles from one topic
+    '''
+    base_url = 'https://inshorts.com/en/read/'
+    response = get(base_url + topic)
+    soup = BeautifulSoup(response.content, 'html.parser')
+    titles = soup.find_all('span', itemprop='headline')
+    summaries = soup.find_all('div', itemprop='articleBody')
+    summary_list = []
+    for i in range(len(titles)):  
+        temp_dict = {'category': topic,
+                     'title': titles[i].text,
+                     'content': summaries[i].text}
+        summary_list.append(temp_dict)
+    return summary_list    
+
+
+#Define a function that will scrape information about an array of topics
+def get_news_articles(topic_list):
+    '''
+    function takes in a list of topics and then returns articles
+    '''
+    file = 'news_articles.json'
+    if os.path.exists(file):
+        with open(file) as f: 
+            return json.load(f)
+    final_list = []
+    for topic in topic_list:
+        final_list.extend(scrape_one_page(topic))
+    with open(file, 'w') as f:
+        json.dump(final_list, f)
+    return final_list    
